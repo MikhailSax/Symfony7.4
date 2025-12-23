2dd913f500c377b96e5af77607eb8aaf299d852e
mv <dominobryanskiy@yandex.ru>
2025-12-23 18:02:52 +0800

    controllers and twig

--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 class LuckyController extends AbstractController
@@ -15,5 +16,11 @@ class LuckyController extends AbstractController
         return $this->render('lucky/number.html.twig', [
             'number' => $number,
         ]);
+    }
+
+    #[Route('/lucky/json', name: 'lucky_json', methods: ['GET'])]
+    public function show(): JsonResponse
+    {
+        return $this->json(['name' => 'misha']);
     }
 }
